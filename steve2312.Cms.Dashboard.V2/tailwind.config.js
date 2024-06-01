8f7a1830ce82959c50d73630d9952c64947cb587
Stefan So <stefanso2312@gmail.com>
2024-06-01 14:11:18 +0200

    Dashboard project

---
+++ steve2312.Cms.Dashboard.V2/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    fontFamily: {
+      sans: [
+        "Poppins",
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
